f009db9c8f025f94f9ee7b5f8793dfb4620ca3be
roger chat <mattrogerpelosi@gmail.com>
2021-09-23 16:25:07 -0400

    commit tree 7a0db4ede891b2f5bb49f277200a1379ede8ad91

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 33, 'F', 'BROWN', 'EASY-GOING', 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'BLACK', 'ROWDYISH', 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 69, 'M', 'PURPLE', 'MAGICAL', 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 421, 'M', 'BROWN', 'DUMB', 0);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'M', 'GREEN', 'EASY-GOING', 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 13, 'F', 'WHITE', 'MEAN' 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 27, 'M', 'JAZZY', 'JAZZY', 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 45, 'F', NULL, NULL, 1);
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
